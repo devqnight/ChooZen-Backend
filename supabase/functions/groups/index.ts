f987f283e1ad4de330a57952bf19b24f8e68fa38
devqnight <23532564+devqnight@users.noreply.github.com>
2025-06-20 23:26:03 +0200

    initialised hono app for functions

--- supabase/functions/groups/index.ts
+++ supabase/functions/groups/index.ts
@@ -1,23 +1,12 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { Hono } from 'jsr:@hono/hono';
+import { SupabaseEnv } from 'honoInit';
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 
-// Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+const app = new Hono<SupabaseEnv>().basePath("/groups");
 
-console.log("Hello from Functions!")
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+Deno.serve(app.fetch);
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
 
 /* To invoke locally:
 
